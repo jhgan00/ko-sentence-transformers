c9e8c1f098ede9dea3b2e4b28a48528a4a33ab86
jhgan <jhgan00@gmail.com>
2021-12-23 21:41:02 +0900

    added multilingual model benchmark

---
+++ benchmark.py
@@ -0,0 +1,42 @@
+import os
+import csv
+import logging
+from sentence_transformers import LoggingHandler, SentenceTransformer, InputExample
+from sentence_transformers.evaluation import EmbeddingSimilarityEvaluator
+
+sts_dataset_path = 'KorNLUDatasets/KorSTS'
+
+logging.basicConfig(
+    format='%(asctime)s - %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S',
+    level=logging.INFO,
+    handlers=[LoggingHandler()]
+)
+
+# Read STSbenchmark dataset and use it as development set
+test_samples = []
+test_file = os.path.join(sts_dataset_path, "sts-test.tsv")
+with open(test_file, 'rt', encoding='utf8') as fIn:
+    reader = csv.DictReader(fIn, delimiter='\t', quoting=csv.QUOTE_NONE)
+    for row in reader:
+        score = float(row['score']) / 5.0  # Normalize score to range 0 ... 1
+        test_samples.append(InputExample(texts=[row['sentence1'], row['sentence2']], label=score))
+test_evaluator = EmbeddingSimilarityEvaluator.from_input_examples(test_samples, name='sts-test')
+
+model = SentenceTransformer("jhgan/ko-sbert-nli")
+test_evaluator(model)
+
+model = SentenceTransformer("jhgan/ko-sbert-sts")
+test_evaluator(model)
+
+model = SentenceTransformer("distiluse-base-multilingual-cased-v1")
+test_evaluator(model)
+
+model = SentenceTransformer("distiluse-base-multilingual-cased-v2")
+test_evaluator(model)
+
+model = SentenceTransformer("paraphrase-multilingual-MiniLM-L12-v2")
+test_evaluator(model)
+
+model = SentenceTransformer("paraphrase-multilingual-mpnet-base-v2")
+test_evaluator(model)
--- setup.py
+++ setup.py
@@ -2,9 +2,12 @@ from glob import glob
 from os.path import basename, splitext
 from setuptools import find_packages, setup
 
+with open("README.md", "r") as f:
+    long_description = f.read()
+
 setup(
     name='ko_sentence_transformers',
-    version='0.1',
+    version='0.3',
     packages=find_packages(where='src'),
     package_dir={'': 'src'},
     author="Junghyun Gan",
@@ -12,5 +15,7 @@ setup(
     install_requires=['sentence-transformers'],
     license="Apache2",
     python_requires=">=3",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     py_modules=[splitext(basename(path))[0] for path in glob('src/ko_sentence_transformers/*.py')],
 )
